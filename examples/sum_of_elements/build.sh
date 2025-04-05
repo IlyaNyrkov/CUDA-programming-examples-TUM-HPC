5666e6aa429712c1ec6f1df6fa731752783cf7ff
ilya.nyrkov <ilya.nyrkov@gmail.com>
2025-04-05 17:29:21 +0200

    fixed build script for sum of elements

--- examples/sum_of_elements/build.sh
+++ examples/sum_of_elements/build.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+# replace for your architecture -arch=sm_90 
 # Build CUDA files in final_sum_on_cpu
 echo "🔧 Building final_sum_on_cpu..."
 for file in final_sum_on_cpu/*.cu; do
     filename=$(basename "$file" .cu)
-    nvcc "$file" -o "final_sum_on_cpu/$filename"
+    nvcc "$file" -O3 -arch=sm_90 -o "final_sum_on_cpu/$filename"
 done
 
 # Build CUDA files in final_sum_on_atomic
 echo "🔧 Building final_sum_on_atomic..."
 for file in final_sum_on_atomic/*.cu; do
     filename=$(basename "$file" .cu)
-    nvcc "$file" -o "final_sum_on_atomic/$filename"
+    nvcc "$file" -O3 -arch=sm_90 -o "final_sum_on_atomic/$filename"
 done
 
 # Build CUDA files in cudaMalloc
 echo "🔧 Building cudaMalloc..."
 for file in cudaMalloc/*.cu; do
     filename=$(basename "$file" .cu)
-    nvcc "$file" -o "final_sum_on_atomic/$filename"
+    nvcc "$file" -O3 -arch=sm_90 -o "cudaMalloc/$filename"
 done
 
 echo "✅ Build complete."
